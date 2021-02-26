f4c9953d8f2e05db76d8ad6e0d8f0f363b3b25fe
Maksim Lopatin <maksim.lopatin.spb@mail.ru>
2021-02-26 16:39:07 +0300

    83.05

--- dtwork/ml.py
+++ dtwork/ml.py
@@ -117,15 +117,18 @@ def cnn(df_train, df_test):
 
     # Создаем последовательную модель
     model = Sequential()
-    model.add(Conv2D(24, (3, 3), padding='same', input_shape=(4, 56, 1), activation='relu'))
-    #model.add(Conv2D(4, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(28, (3, 3), padding='same', input_shape=(4, 56, 1), activation='relu'))
+    model.add(Dropout(0.1))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.15))
-    #model.add(Conv2D(12, (3, 3), padding='same', activation='relu'))
+    model.add(Dropout(0.1))
+    model.add(Conv2D(12, (3, 3), padding='same', activation='relu'))
+    model.add(Dropout(0.1))
 
     model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
-    model.add(Dropout(0.15))
+    model.add(Dense(80, activation='relu'))
+    model.add(Dropout(0.1))
+    model.add(Dense(40, activation='relu'))
+    model.add(Dropout(0.1))
     model.add(Dense(len(obj_lst), activation='softmax'))
 
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
