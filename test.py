8176e535e8051666ac182b56e27f99d67549f1cb
Maksim Lopatin <maksim.lopatin.spb@mail.ru>
2021-06-28 01:49:11 +0300

    rename

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
-import wifi_log.logreader as lr
+from wifi_log.log import Log
 
 
-lr.Log.run_lib('./dll/readcsi.dll')
-log1 = lr.Log('./csi/homelocation/five place/test/bathroom4.dat').read()
+Log.run_lib('./dll/readcsi.dll')
+log1 = Log('./csi/homelocation/five place/test/bathroom4.dat').read()
 print(len(log1))
-log2 = lr.Log('./csi/use_in_paper/2_objects/train/D=2020-01-02_T=21-34-18--air.dat').read()
+log2 = Log('./csi/use_in_paper/2_objects/train/D=2020-01-02_T=21-34-18--air.dat').read()
 print(len(log2))
-
 
 log = log1 + log2
 print(len(log))
-
-
 
 a = 5
