662c669a6a460930d1ef767d2bd1049966ae3705
Maksim Lopatin <maksim.lopatin.spb@mail.ru>
2021-07-01 02:06:45 +0300

    improove example

--- test2.py
+++ test2.py
@@ -6,10 +6,10 @@ mf = MetaWifi('./csi/homelocation/five place')
 print(mf.df_raw)
 # mf.df_raw.drop(['payload', 'csi', 'path'], axis=1).head(1000).to_csv('test.csv', index=False)
 
-df1 = mf.df_raw.groupby(['type', 'category']).count()['field_len']
-df2 = mf.df_raw[mf.df_raw['csi_len'] == 0].groupby(['type', 'category']).count()['csi_len']
+df1 = mf.df_raw.groupby(['type', 'category',]).count()['field_len']
+df2 = mf.df_raw[mf.df_raw['csi_len'] == 0].groupby(['type', 'category',]).count()['csi_len']
 df = pd.concat([df1, df2], axis=1)
-df['wrong_%'] = df2 / df1 * 100
+df['wrong_%'] = (df2 / df1 * 100).astype(int)
 print(df)
 
 
@@ -93,7 +93,7 @@ print(df)
 
 # Значительное ускорение и упрощение (только нативный python) кода + сделал его действительно универсальным (ускорение важно для raspberry)
 
-# Простота кода + менеджмент (поле "time")
+# Простота кода + менеджмент (поле "time" у классов)
 
 # Какой процент пакетов является мусором для каких категории и типа - дополнительный анализ данных
 
