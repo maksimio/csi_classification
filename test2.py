ee8bcbc8db70f298c1b4cd06a1eb184330b925e9
Maksim Lopatin <maksim.lopatin.spb@mail.ru>
2021-07-01 02:00:37 +0300

    add example

--- test2.py
+++ test2.py
@@ -1,12 +1,65 @@
-from re import I
+import pandas as pd
 from metawifi import MetaWifi
 
 mf = MetaWifi('./csi/homelocation/five place')
+
 print(mf.df_raw)
-mf.df_raw.drop(['payload', 'csi', 'path'], axis=1).head(1000).to_csv('test.csv', index=False)
+# mf.df_raw.drop(['payload', 'csi', 'path'], axis=1).head(1000).to_csv('test.csv', index=False)
+
+df1 = mf.df_raw.groupby(['type', 'category']).count()['field_len']
+df2 = mf.df_raw[mf.df_raw['csi_len'] == 0].groupby(['type', 'category']).count()['csi_len']
+df = pd.concat([df1, df2], axis=1)
+df['wrong_%'] = df2 / df1 * 100
+print(df)
 
 
-#TODO: метод, который добывает пути рекурсивно для всех файлов
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#TODO: метод, который добывает пути рекурсивно для всех файлов (и проверка на .dat в конце - пустые ридеры)
 
 
 
