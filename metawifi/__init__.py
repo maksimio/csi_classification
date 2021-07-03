b6bb6619734f1038932e9994a79cfb80864b6206
Maksim Lopatin <maksim.lopatin.spb@mail.ru>
2021-07-04 01:41:05 +0300

    ...

--- metawifi/__init__.py
+++ metawifi/__init__.py
@@ -1,5 +1,6 @@
 #TODO Умное чтение вложенных файлов с отсевом не .dat файлов
 #TODO MetaWifi - методы с информацией о df (число категорий и т.п.)
+from re import I
 from .watcher import Watcher as W
 from .log.logcombiner import LogCombiner
 import pandas as pd
@@ -16,11 +17,18 @@ class MetaWifi(LogCombiner):
         self.__make_df_raw()
         self.__w.hprint(self.__w.INFO, 'MetaWifi: {} to df in ' + str(round(self.time[-1]['duration'], 2)) + ' seconds')
 
-        self.__make_df_raw_filter()
+        self.__filter()
         self.__w.hprint(self.__w.INFO, 'MetaWifi: filter df_raw and remove ' + str(self.df_raw.shape[0] - self.df.shape[0]) + ' packets' )
 
-        self.__make_csi()
+        self.restore_csi()
         self.__w.hprint(self.__w.INFO, 'MetaWifi: get complex CSI dfs in ' + str(round(self.time[-1]['duration'], 2)) + ' seconds')
+
+        self.__num_tones = int(self.df['num_tones'].mean())
+
+        self.__type = 'complex'
+
+        self.__split_csi()
+        self.__concat_csi()
 
 
     @W.stopwatch
@@ -29,16 +37,67 @@ class MetaWifi(LogCombiner):
 
 
     @W.stopwatch
-    def __make_df_raw_filter(self) -> None:
+    def __filter(self) -> None:
         df = self.df_raw
         self.df = df[(df['csi_len'] != 0)].reset_index(drop=True)
 
 
     @W.stopwatch
-    def __make_csi(self) -> None:
+    def restore_csi(self):
         csi = np.array(self.df['csi'].to_list()) #TODO отлавливать ошибку IndexError: tuple index out of range и выводить ее
         self.df_csi_complex = pd.DataFrame(csi.reshape((csi.shape[0], csi.shape[1] * csi.shape[2]))) #TODO заменить to_list на values
-        
         self.df_csi_abs = self.df_csi_complex.abs()
         self.df_csi_phase = pd.DataFrame(np.angle(self.df_csi_complex.values))
 
+        return self
+
+
+    def __split_csi(self) -> None:
+        if self.__type == 'complex':
+            df = self.df_csi_complex
+        elif self.__type == 'abs':
+            df = self.df_csi_abs
+        elif self.__type == 'phase':
+            df = self.df_csi_phase
+
+        self.__df_csi_lst = []
+        for i in range(int(df.shape[1] / self.__num_tones)):
+            item = df[[k + i*self.__num_tones for k in range(0, self.__num_tones)]]
+            self.__df_csi_lst.append(item)
+
+
+    def __concat_csi(self) -> None:
+        if self.__type == 'complex':
+            self.df_csi_complex = pd.concat(self.__df_csi_lst, axis=1)
+        elif self.__type == 'abs':
+            self.df_csi_abs = pd.concat(self.__df_csi_lst, axis=1)
+        elif self.__type == 'phase':
+            self.df_csi_phase = pd.concat(self.__df_csi_lst, axis=1)
+
+
+    def set_type(self, active_type: str):
+        if active_type == 'complex' or active_type == 'abs' or active_type == 'phase':
+            self.__type = active_type
+            self.__w.hprint(self.__w.SUCCESS, 'MetaWifi: change type to ' + active_type)
+            return self
+        else:
+            self.__w.hprint(self.__w.FAIL, 'MetaWifi: wrong active type in set_type! Exit...')
+            exit()
+
+
+    def get_type(self) -> str:
+        return self.__type
+        
+    
+    def smooth(self, window: int=5, win_type: str=None):
+        '''https://docs.scipy.org/doc/scipy/reference/signal.windows.html#module-scipy.signal.windows'''
+        if self.__type == 'complex':
+            self.__w.hprint(self.__w.FAIL, 'MetaWifi: in smooth active type can`t be complex! Exit...')
+            exit()
+        self.__split_csi()
+
+        for i in range(len(self.__df_csi_lst)):
+            self.__df_csi_lst[i] = self.__df_csi_lst[i].T.rolling(window, min_periods=1, center=True, win_type=win_type).mean().T
+
+        self.__concat_csi()
+        return self
--- test2.py
+++ test2.py
@@ -2,6 +2,8 @@ import pandas as pd
 from metawifi import MetaWifi
 from matplotlib import pyplot as plt
 
-mf = MetaWifi('./csi/use_in_paper/2_objects')
-
-print(mf.df.groupby('type', axis=0).count())
+mf = MetaWifi('./csi/use_in_paper/4_objects')
+mf.set_type('abs').smooth(20, 'hamming')
+print(mf.df_csi_complex)
+mf.df_csi_abs.head(20).T.plot()
+plt.show()
